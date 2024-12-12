6f444f99f8c57d7560d4b74ccd1e8f3020edf188
Evgeniy <96259758+SayJustOnlyMe@users.noreply.github.com>
2024-12-12 22:50:37 +0300

    Update queries.sql

--- SQL/Yandex Workshop/SQL for data science and analytics/Module 1/queries.sql
+++ SQL/Yandex Workshop/SQL for data science and analytics/Module 1/queries.sql
@@ -16,3 +16,12 @@ FROM company
 WHERE category_code = 'news'
 AND country_code = 'USA'
 ORDER BY funding_total DESC;
+
+-- Запрос 3:
+
+-- Найдите общую сумму сделок по покупке одних компаний другими в долларах. Отберите сделки, которые осуществлялись только за наличные с 2011 по 2013 год включительно.
+
+SELECT SUM(price_amount)
+FROM acquisition
+WHERE term_code = 'cash'
+AND EXTRACT(YEAR FROM acquired_at) IN (2011, 2012, 2013);
