0fae6d16c3cfcddea8934b78dadb9fd6db29a9a8
Evgeniy <96259758+SayJustOnlyMe@users.noreply.github.com>
2024-12-12 22:58:33 +0300

    Update queries.sql

--- SQL/Yandex Workshop/SQL for data science and analytics/Module 1/queries.sql
+++ SQL/Yandex Workshop/SQL for data science and analytics/Module 1/queries.sql
@@ -35,3 +35,12 @@ SELECT first_name,
        network_username
 FROM people
 WHERE network_username LIKE 'Silver%';
+
+-- Запрос 5:
+
+-- Выведите на экран всю информацию о людях, у которых названия аккаунтов в поле network_username содержат подстроку 'money', а фамилия начинается на 'K'.
+
+SELECT *
+FROM people
+WHERE network_username LIKE '%money%'
+AND last_name LIKE 'K%';
