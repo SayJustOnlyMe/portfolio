9d7e12455b64eb6a30068b35281f611ab808e9b8
Evgeniy <96259758+SayJustOnlyMe@users.noreply.github.com>
2024-12-12 23:48:34 +0300

    Update queries.sql

--- SQL/Yandex Workshop/SQL for data science and analytics/Module 1/queries.sql
+++ SQL/Yandex Workshop/SQL for data science and analytics/Module 1/queries.sql
@@ -83,3 +83,19 @@ SELECT *,
            WHEN invested_companies < 20 THEN 'low_activity'
        END
 FROM fund;
+
+-- Запрос 9:
+
+-- Для каждой из категорий, назначенных в предыдущем задании, посчитайте округлённое до ближайшего целого числа среднее количество инвестиционных раундов, в которых фонд принимал участие. 
+-- Выведите на экран категории и среднее число инвестиционных раундов. Отсортируйте таблицу по возрастанию среднего.
+
+SELECT 
+       CASE
+           WHEN invested_companies>=100 THEN 'high_activity'
+           WHEN invested_companies>=20 THEN 'middle_activity'
+           ELSE 'low_activity'
+       END AS activity,
+       ROUND(AVG(investment_rounds)) AS avg
+FROM fund
+GROUP BY activity
+ORDER BY avg ASC;
