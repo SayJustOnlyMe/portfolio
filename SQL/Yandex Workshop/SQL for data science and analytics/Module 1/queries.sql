df7c43450ba100e24fdba9d1179c35b2ffa406a8
Evgeniy <96259758+SayJustOnlyMe@users.noreply.github.com>
2024-12-13 00:26:40 +0300

    Update queries.sql

--- SQL/Yandex Workshop/SQL for data science and analytics/Module 1/queries.sql
+++ SQL/Yandex Workshop/SQL for data science and analytics/Module 1/queries.sql
@@ -99,3 +99,22 @@ SELECT
 FROM fund
 GROUP BY activity
 ORDER BY avg ASC;
+
+-- Запрос 10:
+
+-- Проанализируйте, в каких странах находятся фонды, которые чаще всего инвестируют в стартапы. 
+-- Для каждой страны посчитайте минимальное, максимальное и среднее число компаний, в которые инвестировали фонды этой страны, основанные с 2010 по 2012 год включительно. 
+-- Исключите страны с фондами, у которых минимальное число компаний, получивших инвестиции, равно нулю. 
+-- Выгрузите десять самых активных стран-инвесторов: отсортируйте таблицу по среднему количеству компаний от большего к меньшему. Затем добавьте сортировку по коду страны в лексикографическом порядке.
+
+SELECT 
+       country_code,
+       MIN(invested_companies) AS min,
+       MAX(invested_companies) AS max,
+       AVG(invested_companies) AS avg
+FROM fund
+WHERE EXTRACT(YEAR FROM founded_at) BETWEEN 2010 AND 2012
+GROUP BY country_code
+HAVING MIN(invested_companies) > 0
+ORDER BY avg DESC, country_code ASC
+LIMIT 10;
