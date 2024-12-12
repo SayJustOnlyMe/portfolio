3fde6d536fed93a98fe448bed5bb3b0a51b1c5f6
Evgeniy <96259758+SayJustOnlyMe@users.noreply.github.com>
2024-12-12 23:30:17 +0300

    Update queries.sql

--- SQL/Yandex Workshop/SQL for data science and analytics/Module 1/queries.sql
+++ SQL/Yandex Workshop/SQL for data science and analytics/Module 1/queries.sql
@@ -69,3 +69,17 @@ FROM funding_round
 GROUP BY funded_at
 HAVING  MIN(raised_amount) != 0
 AND MIN(raised_amount) != MAX(raised_amount);
+
+-- Запрос 8:
+
+-- Создайте поле с категориями: Для фондов, которые инвестируют в 100 и более компаний, назначьте категорию *high_activity*. 
+-- Для фондов, которые инвестируют в 20 и более компаний до 100, назначьте категорию *middle_activity*. 
+-- Если количество инвестируемых компаний фонда не достигает 20, назначьте категорию *low_activity*. Отобразите все поля таблицы *fund* и новое поле с категориями.
+
+SELECT *,
+       CASE
+           WHEN invested_companies >= 100 THEN 'high_activity'
+           WHEN invested_companies BETWEEN 20 AND 100 THEN 'middle_activity'
+           WHEN invested_companies < 20 THEN 'low_activity'
+       END
+FROM fund;
