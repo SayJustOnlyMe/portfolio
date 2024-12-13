3ca0a89c88354f7b522b8e43fb07e35ec4c48b7a
Evgeniy <96259758+SayJustOnlyMe@users.noreply.github.com>
2024-12-13 17:00:40 +0300

    Update queries.sql

--- SQL/Yandex Workshop/SQL for data science and analytics/Module 1/queries.sql
+++ SQL/Yandex Workshop/SQL for data science and analytics/Module 1/queries.sql
@@ -287,3 +287,15 @@ GROUP BY name_of_fund, name_of_company, amount;
 -- доля, которая отображает, во сколько раз сумма покупки превысила сумму вложенных в компанию инвестиций, округлённая до ближайшего целого числа.
 -- Не учитывайте те сделки, в которых сумма покупки равна нулю. Если сумма инвестиций в компанию равна нулю, исключите такую компанию из таблицы. 
 -- Отсортируйте таблицу по сумме сделки от большей к меньшей, а затем по названию купленной компании в лексикографическом порядке. Ограничьте таблицу первыми десятью записями.
+
+SELECT c1.name AS acquiring,
+       a.price_amount,
+       c2.name AS acquired,
+       c2.funding_total,
+       ROUND(a.price_amount / c2.funding_total) AS persent
+FROM acquisition AS a
+INNER JOIN company c1 ON a.acquiring_company_id = c1.id
+INNER JOIN company c2 ON a.acquired_company_id = c2.id
+WHERE a.price_amount != 0 AND c2.funding_total != 0
+ORDER BY a.price_amount DESC, c2.name ASC
+LIMIT 10;
